7b811362e378afd3be862e6f776254290c991352
amrithakarthikeyan <amrithakv4s@gmail.com>
2025-06-24 21:15:27 +0000

    commits

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,10 @@ const sqlite3 = require('sqlite3').verbose();
 const app = express();
 const PORT = 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://opulent-barnacle-q76gw546vqvpfj59-8080.app.github.dev'
+}));
+
 app.use(express.json());
 
 const db = new sqlite3.Database('./database.db');
