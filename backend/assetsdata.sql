cefad50b7d99e10525ea71e1e1afbdf2ce8b887f
amrithakarthikeyan <amrithakv4s@gmail.com>
2025-07-07 11:45:34 +0000

    Inserting predefined employees list- Schema.sql

--- backend/assetsdata.sql
+++ backend/assetsdata.sql
@@ -9,17 +9,6 @@ CREATE TABLE "Assets" (
 	PRIMARY KEY("Asset-ID")
 );
 
-CREATE TABLE "Assignment" (
-	"Assignment_ID"	INTEGER NOT NULL PRIMARY KEY,
-	"Asset-ID"	INTEGER NOT NULL,
-	"Employee_ID"	INTEGER NOT NULL,
-	"Assigned_Date"	TEXT,
-	"Return_Date"	TEXT,
-	"Condition_on_return"	TEXT,
-	FOREIGN KEY("Asset-ID") REFERENCES "Assets"("Asset-ID") ,
-	FOREIGN KEY("Employee_ID") REFERENCES "Employees"("Employee-ID")
-);
-
 CREATE TABLE "Employees" (
 	"Employee-ID"	INTEGER NOT NULL,
 	"Name"	TEXT NOT NULL,
@@ -27,3 +16,15 @@ CREATE TABLE "Employees" (
 	"Email"	TEXT UNIQUE,
 	PRIMARY KEY("Employee-ID")
 );
+
+INSERT INTO Employees ("Employee-ID", Name, Department, Email) VALUES
+(4504, 'Aaron Williams', 105, 'mary72@gmail.com'),
+(4029, 'Angela Sanchez', 106, 'nancy45@gmail.com'),
+(8855, 'Cynthia Cunningham', 103, 'johnwilliams@yahoo.com'),
+(2036, 'Kaitlyn Cooper', 105, 'gilbertkathleen@gmail.com'),
+(6374, 'Brian Massey', 101, 'kathrynsmith@salazar-jackson.com'),
+(6433, 'Melissa Graham DDS', 104, 'kerickson@hotmail.com'),
+(8002, 'Kenneth Bass', 108, 'bwarren@hotmail.com'),
+(2440, 'Michael Gutierrez', 104, 'enichols@lee.com'),
+(2039, 'Amber Smith', 102, 'callahanjennifer@smith.com');
+
